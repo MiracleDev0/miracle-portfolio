3d3edad0839247e649a19ef79365b63276c513a2
Esan Miracle <miracleesan7@gmail.com>
2025-07-08 10:07:56 +0100

    added the resume icon

--- src/components/Hero-Section/HeroSection.tsx
+++ src/components/Hero-Section/HeroSection.tsx
@@ -13,7 +13,14 @@ const HeroSection: React.FC = () => {
             ☰
           </div>
           <div className="nav-actions">
-            <a href="#" className="resume-link">Resume</a>
+            <div className="resume">
+              <svg xmlns="http://www.w3.org/2000/svg" cursor="pointer" width="1em" height="1em" viewBox="0 0 24 24" className="fill-white"><path fill="currentColor" d="M14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3m-2 16H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7h-2v7Z"></path></svg>
+              <a 
+                href="#" className="resume-link"
+                >
+                Resume
+              </a>
+            </div>
             <a href="#contact"><button className="contact-btn">Contact Me</button></a>
           </div>
         </div>
