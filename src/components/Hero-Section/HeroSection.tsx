3fa61650f4296aeaa9be3e3aea0f91b38799a8d2
Esan Miracle <miracleesan7@gmail.com>
2025-07-17 15:04:25 +0100

    updated the link to download resume

--- src/components/Hero-Section/HeroSection.tsx
+++ src/components/Hero-Section/HeroSection.tsx
@@ -16,7 +16,12 @@ const HeroSection: React.FC = () => {
             <div className="resume">
               <svg xmlns="http://www.w3.org/2000/svg" cursor="pointer" width="1em" height="1em" viewBox="0 0 24 24" className="fill-white"><path fill="currentColor" d="M14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3m-2 16H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7h-2v7Z"></path></svg>
               <a 
-                href="#" className="resume-link"
+                href="https://drive.google.com/file/d/1jotWdSsTbumHtZy6NKVIM3nHvofgnDR7/view?usp=drive_link"
+                target="_blank"
+                rel="noopener noreferrer"
+                // download
+                aria-label="Download Resume" 
+                className="resume-link"
                 >
                 Resume
               </a>
